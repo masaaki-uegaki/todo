a874efbdb73a34ac653294dd370848aa8cbc5aa5
上垣正顕 <uwettie@uegakimasaakinoiMac.local>
2018-04-18 16:28:43 +0900

    change types

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,18 +1,42 @@
-declare module app {
-  type Error = {
-    message: string;
-    status: number;
-  }
-  namespace model {
-    type User = {
-      uid: string;
-      email: string;
-      displayName?: string;
-      photoURL?: string;
-      profile?: string;
-    };
-  }
-}
+import { Moment } from 'moment';
 
-declare module todo {
+declare global {
+  namespace app {
+    type Error = {
+      message: string;
+      status: number;
+    }
+
+    namespace model {
+      type User = {
+        uid: string;
+        email: string;
+        displayName?: string;
+        photoURL?: string;
+        profile?: string;
+      };
+    }
+  }
+
+  namespace todo {
+    namespace model {
+      type Task = {
+        id: string;
+        name: string;
+        desc?: string;
+        priority?: string;
+        startAt?: string | Moment;
+        dueAt?: string | Moment;
+        expectedManHour?: number;
+        actualManHour?: number;
+        expectedCost?: number;
+        actualCost?: number;
+        progress?: number;
+        createdAt: string | Moment;
+        createdUid: string;
+        modifiedAt?: string | Moment;
+        modifiedUid?: string;
+      }
+    }
+  }
 }
